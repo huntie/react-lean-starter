2529570ab3531b6517e1bfa824df728c5a91a9f5
Alex Hunt <hello@alexhunt.dev>
2025-02-16 15:26:54 +0000

    Add React Compiler

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,10 @@
 // https://eslint.org/docs/latest/use/configure/
 
 import react from 'eslint-plugin-react';
+import reactCompiler from 'eslint-plugin-react-compiler';
 
 export default [
+  reactCompiler.configs.recommended,
   {
     plugins: {
       react,
--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,12 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react({
+      babel: {
+        plugins: ['babel-plugin-react-compiler'],
+      },
+    }),
+    tailwindcss(),
+  ],
 });
